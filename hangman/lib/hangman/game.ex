d4e53c960989d860ee5052c68779bf685b4a8862
Pedro Gaspar <pedro.mig.gaspar@gmail.com>
2021-07-07 21:03:36 +0100

    Fix game

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -10,7 +10,8 @@ defmodule Hangman.Game do
   end
 
   def new_game() do
-    new_game(Dictionary.random_word())
+    words = Dictionary.start()
+    new_game(Dictionary.random_word(words))
   end
 
   def make_move(game = %{game_state: state}, _guess) when state in [:won, :lost] do
